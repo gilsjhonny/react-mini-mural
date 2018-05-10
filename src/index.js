304fb12217d510f62d00ba60fbabd72cb9bd0c80
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-09 21:17:58 -0300

    Delete creact-react-app sample files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const App = () => <h1>Mural</h1>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
