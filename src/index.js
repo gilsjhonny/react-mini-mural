edcdaa2d7fa7544089939913d28941b639a6954a
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-10 00:53:48 -0300

    Set up eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
 
 const App = () => <h1>Mural</h1>;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
