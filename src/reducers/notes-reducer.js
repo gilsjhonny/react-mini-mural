09ec06150d9a5004a2f663a821b50c7a6cebb556
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-10 23:17:13 -0300

    Update notes-reducer with new actions

--- src/reducers/notes-reducer.js
+++ src/reducers/notes-reducer.js
@@ -1,5 +1,12 @@
-export default function(state = {}, action) {
+import { ADD_NEW_NOTE, REMOVE_NOTE } from "../actionTypes";
+
+export default function(state = [], action) {
   switch (action.type) {
+    case ADD_NEW_NOTE:
+      const { id, text } = action;
+      return [...state, { id, text }];
+    case REMOVE_NOTE:
+      return state.filter(note => note.id !== action.id);
     default:
       return state;
   }
