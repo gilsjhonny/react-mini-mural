67b842286e107af8c6041c6ce84a527d89f3d3f1
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-10 22:19:25 -0300

    Add StickyNote component

--- src/components/StickyNote/StickyNote.jsx
+++ src/components/StickyNote/StickyNote.jsx
@@ -1,9 +1,33 @@
 import React from "react";
-
+import PropTypes from "prop-types";
+import "./styles.css";
 class StickyNote extends React.Component {
+  static propTypes = {
+    color: PropTypes.number.string,
+    height: PropTypes.number.string,
+    width: PropTypes.number.string
+  };
+
+  static defaultProps = {
+    color: "#b1d6d0",
+    height: "100px",
+    width: "100px",
+    top: "0",
+    left: "0"
+  };
+
   render() {
+    const { color, height, width, top, left } = this.props;
     return (
-      <div style={{ width: "200px", height: "200px", background: "mistyrose" }}>
+      <div
+        className="StickyNote"
+        style={{
+          width,
+          height,
+          background: color,
+          transform: `translate(${top},${left})`
+        }}
+      >
         Some Note
       </div>
     );
