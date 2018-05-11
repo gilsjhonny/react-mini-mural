c74fbfe93bfd3a4463ab32f496830bb510a22ede
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-10 23:18:29 -0300

    Update initial-state

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -1,4 +1,9 @@
-const notes = [];
+import uniqueId from "lodash/uniqueId";
+
+const notes = [
+  { text: "Hola", id: uniqueId() },
+  { text: "Hola 2", id: uniqueId() }
+];
 
 export default {
   notes
