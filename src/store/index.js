8a43e2d58649870ace9e9ba01b493c3b665872d1
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-11 23:09:05 -0300

    Enable Redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -2,4 +2,8 @@ import { createStore } from "redux";
 import reducers from "../reducers";
 import initialState from "./initial-state";
 
-export default createStore(reducers, initialState);
+export default createStore(
+  reducers,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
