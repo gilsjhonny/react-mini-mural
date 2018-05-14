b2eb25d4a521129c17e475dec8be9f56ab74a792
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-13 22:28:39 -0300

    Remove comment

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -1,19 +1,3 @@
-// const initialNotes = {
-//   "1": {
-//     id: "1",
-//     text: "Double click to add Sticky Notes 😀",
-//     width: "140px",
-//     height: "140px",
-//     color: "rgb(242, 214, 255)",
-//     x: "100",
-//     y: "100"
-//   }
-// };
-
-// const selected_notes = {
-//   "1": { note_id: 1 }
-// };
-
 export default {
   notes: {},
   mural: {
