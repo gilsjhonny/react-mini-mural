319524433daee6d7adac8ebc8d9998c39b03de4e
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-15 01:16:18 -0300

    Remove unnecesary rewire

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-/* config-overrides.js */
-const { injectBabelPlugin } = require("react-app-rewired");
-
-module.exports = function override(config, env) {
-  // add a plugin
-  const myPlugins = ["transform-decorators-legacy"];
-
-  return injectBabelPlugin(myPlugins, config);
-};
