0385dc1269a733ae3d02c1cbbb0cc83684ca1ae0
Jhonny Gil <gilsjhonny@gmail.com>
2018-05-15 10:14:56 -0300

    Update toolbar and colorpicker

--- src/components/Toolbar/ColorPicker/ColorBox/ColorBox.js
+++ src/components/Toolbar/ColorPicker/ColorBox/ColorBox.js
@@ -1,11 +1,34 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Color from "color";
 import classnames from "classnames";
 import "./styles.css";
 
 class ColorBox extends React.Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+    color: PropTypes.string.isRequired
+  };
+
   handleClick = e => {
     const color = e.target.dataset.color;
     this.props.onClick(color);
+  };
+
+  getBorderStyle = () => {
+    const { color, active } = this.props;
+    let border;
+
+    if (active) {
+      const borderColor = Color(color)
+        .darken(0.4)
+        .desaturate(0.2);
+      border = `2px solid ${borderColor}`;
+    } else {
+      border = "1px solid #bbbbbb";
+    }
+
+    return border;
   };
 
   render() {
@@ -17,7 +40,7 @@ class ColorBox extends React.Component {
         className={className}
         onClick={this.handleClick}
         data-color={color}
-        style={{ background: color }}
+        style={{ background: color, border: this.getBorderStyle() }}
       />
     );
   }
